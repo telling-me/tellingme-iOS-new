bc46e036afb1d505a03310fa6ecb5901b0ad6788
Kyoungmi <1mmgm.dev@gmail.com>
2024-06-01 21:30:52 +0900

    [feat]: project init

---
+++ tellingme/tellingme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tellingme
+//
+//  Created by 마경미 on 01.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
