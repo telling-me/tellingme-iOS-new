65e6aaa57d41e331564292c7a404bc86eec47acc
Kyoungmi <1mmgm.dev@gmail.com>
2024-06-12 03:25:32 +0900

    feature: color design system app

---
+++ Projects/AppCore/DesignSystem/Sources/Foundation/Colors/UIColor+.swift
@@ -0,0 +1,47 @@
+//
+//  UIColor+.swift
+//  AppCoreKit
+//
+//  Created by 마경미 on 12.06.24.
+//  Copyright © 2024 tellingUs. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    typealias Colors = AppCoreDesignSystemAsset.Colors
+    public static let background100 = Colors.background100.color
+    public static let background200 = Colors.background200.color
+    public static let background300 = Colors.background300.color
+    public static let base00 = Colors.base00.color
+    public static let base100 = Colors.base100.color
+    public static let gray100 = Colors.gray100.color
+    public static let gray200 = Colors.gray200.color
+    public static let gray300 = Colors.gray300.color
+    public static let gray400 = Colors.gray400.color
+    public static let gray50 = Colors.gray50.color
+    public static let gray500 = Colors.gray500.color
+    public static let gray600 = Colors.gray600.color
+    public static let gray700 = Colors.gray700.color
+    public static let gray800 = Colors.gray800.color
+    public static let gray900 = Colors.gray900.color
+    public static let primary100 = Colors.primary100.color
+    public static let primary200 = Colors.primary200.color
+    public static let primary300 = Colors.primary300.color
+    public static let primary400 = Colors.primary400.color
+    public static let primary50 = Colors.primary50.color
+    public static let primary500 = Colors.primary500.color
+    public static let primary600 = Colors.primary600.color
+    public static let primary700 = Colors.primary700.color
+    public static let primary800 = Colors.primary800.color
+    public static let profileCard100 = Colors.profileCard100.color
+    public static let red100 = Colors.red100.color
+    public static let red200 = Colors.red200.color
+    public static let red300 = Colors.red300.color
+    public static let red400 = Colors.red400.color
+    public static let red50 = Colors.red50.color
+    public static let red500 = Colors.red500.color
+    public static let red600 = Colors.red600.color
+    public static let red700 = Colors.red700.color
+    public static let sub100 = Colors.sub100.color
+}
