373d75bb0610be417c7f26bc9071c1232f067a02
samsung-ga <maxwodyd98@gmail.com>
2024-06-17 13:50:32 +0900

    feat: boxButton 오른쪽 아이콘 인터페이스 추가

--- Projects/AppCore/DesignSystem/Sources/Components/Buttons/BoxButton/BoxButton+Attributes.swift
+++ Projects/AppCore/DesignSystem/Sources/Components/Buttons/BoxButton/BoxButton+Attributes.swift
@@ -12,37 +12,121 @@ import Foundation
 
 extension BoxButton {
     public struct Attributes {
-        public var text: String?
         public let style: BoxButton.Style
         public let size: BoxButton.Size
 
         public init(
-            text: String? = nil,
             style: BoxButton.Style,
-            size: BoxButton._Size
+            size: BoxButton.Size
         ) {
-            self.text = text
             self.style = style
-            self.size = size.convertToSize(style: style)
+            self.size = size
         }
     }
+}
+
+extension BoxButton.Attributes {
+    public static let primaryLarge: Self = .init(
+        style: .primary,
+        size: .large
+    )
+ 
+    public static let primaryMedium: Self = .init(
+        style: .primary,
+        size: .medium
+    )
+
+    public static let primarySmall: Self = .init(
+        style: .primary,
+        size: .small
+    )
+
+    public static let primaryLightLarge: Self = .init(
+        style: .primaryLight,
+        size: .large
+    )
+
+    public static let primaryLightMedium: Self = .init(
+        style: .primaryLight,
+        size: .medium
+    )
+
+    public static let primaryLightSmall: Self = .init(
+        style: .primaryLight,
+        size: .small
+    )
+
+    public static let secondaryLarge: Self = .init(
+        style: .secondary,
+        size: .large
+    )
+
+    public static let secondaryMedium: Self = .init(
+        style: .secondary,
+        size: .medium
+    )
+
+    public static let secondarySmall: Self = .init(
+        style: .secondary,
+        size: .small
+    )
+
+    public static let singleLarge: Self = .init(
+        style: .single,
+        size: .singleLarge
+    )
+
+    public static let singleMedium: Self = .init(
+        style: .single,
+        size: .singleMedium
+    )
+
+    public static let singleSmall: Self = .init(
+        style: .single,
+        size: .singleSmall
+    )
+
+    public static let singleBlackLarge: Self = .init(
+        style: .singleBlack,
+        size: .singleLarge
+    )
+
+    public static let singleBlackMedium: Self = .init(
+        style: .singleBlack,
+        size: .singleMedium
+    )
+
+    public static let singleBlackSmall: Self = .init(
+        style: .singleBlack,
+        size: .singleSmall
+    )
+
+    public static let singleBlackIconLarge: Self = .init(
+        style: .singleBlack,
+        size: .singleIconLarge
+    )
+
+    public static let singleBlackIconMedium: Self = .init(
+        style: .singleBlack,
+        size: .singleIconMedium
+    )
+
+    public static let singleBlackIconSmall: Self = .init(
+        style: .singleBlack,
+        size: .singleIconSmall
+    )
 }
 
 // MARK: - Size
 
 extension BoxButton {
-    public enum _Size {
-        case large
-        case medium
-        case small
-    }
-
     public struct Size: Hashable {
         let height: CGFloat // Note: height는 사용되지 않으나 높이가 제대로 측정되지 않을 경우 속성을 적용합니다.
         let fontStyle: Font.Style
         let cornerRadius: CGFloat
         let verticalSpacing: CGFloat
         let horizontalSpacing: CGFloat
+        var imageSpacing: CGFloat = 8 // Note: default는 8로 설정합니다.
     }
 }
 
@@ -94,40 +178,33 @@ extension BoxButton.Size {
         verticalSpacing: 6,
         horizontalSpacing: 10
     )
-}
 
-// MARK: Converting (_Size -> Size)
+    public static let singleIconLarge: Self = .init(
+        height: 50,
+        fontStyle: .body_01_B,
+        cornerRadius: 12,
+        verticalSpacing: 13,
+        horizontalSpacing: 10,
+        imageSpacing: 8
+    )
 
-extension BoxButton._Size {
-    private var toSingleSize: BoxButton.Size {
-        switch self {
-        case .large:
-            return .singleLarge
-        case .medium:
-            return .singleMedium
-        case .small:
-            return .singleSmall
-        }
-    }
+    public static let singleIconMedium: Self = .init(
+        height: 36,
+        fontStyle: .body_02_B,
+        cornerRadius: 12,
+        verticalSpacing: 6,
+        horizontalSpacing: 10,
+        imageSpacing: 8
+    )
 
-    private var toSize: BoxButton.Size {
-        switch self {
-        case .large:
-            return .large
-        case .medium:
-            return .medium
-        case .small:
-            return .small
-        }
-    }
-
-    func convertToSize(style: BoxButton.Style) -> BoxButton.Size {
-        if style == .single, style == .singleBlack {
-            return self.toSingleSize
-        } else {
-            return self.toSize
-        }
-    }
+    public static let singleIconSmall: Self = .init(
+        height: 30,
+        fontStyle: .caption_01_B,
+        cornerRadius: 8,
+        verticalSpacing: 5,
+        horizontalSpacing: 10,
+        imageSpacing: 8
+    )
 }
 
 // MARK: - Style
--- Projects/AppCore/DesignSystem/Sources/Components/Buttons/BoxButton/BoxButton.swift
+++ Projects/AppCore/DesignSystem/Sources/Components/Buttons/BoxButton/BoxButton.swift
@@ -9,37 +9,40 @@
 import UIKit
 
 public final class BoxButton: BaseButton {
+    private var text: String?
+    private var leftIcon: Icon.Attributes?
+    private var rightIcon: Icon.Attributes?
     private let attributes: BoxButton.Attributes
     
-    private var size: BoxButton.Size {
-        return attributes.size
-    }
-    
-    private var style: BoxButton.Style {
-        return attributes.style
-    }
-    
-    public init(attributes: BoxButton.Attributes) {
+    public init(
+        text: String?,
+        leftIcon: Icon.Attributes? = nil,
+        rightIcon: Icon.Attributes? = nil,
+        attributes: BoxButton.Attributes
+    ) {
+        self.text = text
+        self.leftIcon = leftIcon
+        self.rightIcon = rightIcon
         self.attributes = attributes
-        super.init()
         
+        super.init()
+
         configure()
     }
-    
+
     public convenience init(
         text: String?,
-        style: BoxButton.Style,
-        size: BoxButton._Size
+        icon: Icon.Attributes? = nil,
+        attributes: BoxButton.Attributes
     ) {
         self.init(
-            attributes: .init(
-                text: text,
-                style: style,
-                size: size
-            )
+            text: text,
+            leftIcon: icon,
+            rightIcon: icon,
+            attributes: attributes
         )
     }
-    
+
     public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -53,11 +56,13 @@ extension BoxButton {
         configureContentInsets()
         configureBackgroundColor()
         configureBorder()
+        configureLeftIcon()
+        configureRightIcon()
     }
     
     private func configureAttributedText() {
-        guard let text = attributes.text else { return }
-        
+        guard let text else { return }
+
         let normal: Font.Attributes = .init(
             style: size.fontStyle,
             textColor: style.normalColorSet.textColor
@@ -84,6 +89,7 @@ extension BoxButton {
     private func configureContentInsets() {
         setVerticalSpacing(size.verticalSpacing)
         setHorizontalSpacing(size.horizontalSpacing)
+        setImageSpacing(size.imageSpacing)
     }
     
     private func configureBackgroundColor() {
@@ -103,5 +109,39 @@ extension BoxButton {
             disabled: style.disabledColorSet.borderColor,
             selected: style.selectedColorSet.borderColor
         )
+    }
+
+    private func configureLeftIcon() {
+        guard let leftIcon else { return }
+
+        setImage(
+            normal: .icon(attributes: leftIcon),
+            highlighted: .icon(attributes: leftIcon),
+            disabled: .icon(icon: leftIcon.icon, size: leftIcon.size, color: .gray100),
+            selected: .icon(attributes: leftIcon)
+        )
+    }
+
+    private func configureRightIcon() {
+        guard let rightIcon else { return }
+
+        setRightImage(
+            normal: .icon(attributes: rightIcon),
+            highlighted: .icon(attributes: rightIcon),
+            disabled: .icon(icon: rightIcon.icon, size: rightIcon.size, color: .gray100),
+            selected: .icon(attributes: rightIcon)
+        )
+    }
+}
+
+// MARK: - Utils
+
+extension BoxButton {
+    private var size: BoxButton.Size {
+        return attributes.size
+    }
+
+    private var style: BoxButton.Style {
+        return attributes.style
     }
 }
