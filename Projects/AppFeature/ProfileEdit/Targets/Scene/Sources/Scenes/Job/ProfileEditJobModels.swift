75b12ba5935e1b1711ce9acd2f27db2023701d17
samsung-ga <maxwodyd98@gmail.com>
2024-06-29 00:15:02 +0900

    feat: 직업 선택 화면 UI 및 로직 구현

--- Projects/AppFeature/ProfileEdit/Targets/Scene/Sources/Scenes/Job/ProfileEditJobModels.swift
+++ Projects/AppFeature/ProfileEdit/Targets/Scene/Sources/Scenes/Job/ProfileEditJobModels.swift
@@ -11,5 +11,16 @@ import Foundation
 import AppCore_Entity
 
 enum ProfileEditJob {
-    
+    // MARK: - Entity
+
+    // MARK: - ViewModel
+
+    enum Job: String, CaseIterable {
+        case 중고등학생
+        case 대학생
+        case 취업준비생
+        case 직장인
+        case 주부
+        case 기타
+    }
 }
